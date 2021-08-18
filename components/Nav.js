ba50e39b2d8066128f07b77ab98ba762045b32ed
Daehan <dh16931@gmail.com>
2021-08-18 18:23:05 +0900

    feat: responsive nav with spacer

--- components/Nav.js
+++ components/Nav.js
@@ -1,22 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Text = styled.h1`
-font-size: 20px;
-color: ${({ theme }) => theme.colors.primary};
-// ${props => props.theme.media.desktop`
-//   font-size: ${props.isEmpty ? '20px' : '.8rem'};
-// `}
-// ${props => props.theme.media.tablet`
-//   font-size: ${props.isEmpty ? '15px' : '.8rem'};
-// `}
-${props => props.theme.media.phone`
-  font-size: ${props.isEmpty ? '10px' : '.8rem'};
-`}
-`
+const Spacer = styled.div`
+  ${(props) => props.theme.media.desktop`
+    height: 80px;
+  `}
+  ${(props) => props.theme.media.tablet`
+    height: 70px;
+  `}
+  ${(props) => props.theme.media.phone`
+    height: 60px;
+  `}
+`;
+
+const Container = styled(Spacer)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: fixed;
+  background-color: ${({ theme }) => theme.colors.primary.dark};
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: ${({ theme }) => theme.colors.primary.text};
+  ${(props) => props.theme.media.desktop`
+    font-size: 30px;
+  `}
+  ${(props) => props.theme.media.tablet`
+    font-size: 25px;
+  `}
+  ${(props) => props.theme.media.phone`
+    font-size: 20px;
+  `}
+`;
+
 const Nav = () => (
-  <div>
-      <Text>Fox Lee Fur Fellas</Text>
-  </div>
-)
+  <>
+    <Container>
+      <Title>Fox Lee Fur Fellas</Title>
+    </Container>
+    <Spacer />
+  </>
+);
 
-export default Nav
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import styled from 'styled-components'
-import Nav from '../components/Nav'
+import styled from "styled-components";
+import Nav from "../components/Nav";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <>
-    <Title>My page</Title>
-    <Nav />
-  </>
+  return (
+    <>
+      <Nav />
+      <Title>My page</Title>
+    </>
+  );
 }
