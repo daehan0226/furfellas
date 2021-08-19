cac028725d5f95b3eaadad26a9c2d1aa2c70f32b
Daehan <dh16931@gmail.com>
2021-08-19 22:53:51 +0900

    fix: add missing key

--- components/common/Select.js
+++ components/common/Select.js
@@ -2,7 +2,7 @@ const Select = ({ options }) => {
   return (
     <select value="1">
       {options.map(([value, option]) => (
-        <option value={value}>{option}</option>
+        <option key={value} value={value}>{option}</option>
       ))}
     </select>
   );
