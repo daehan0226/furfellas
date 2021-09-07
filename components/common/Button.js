6eaafa785df9e7af92476daa75c3bca53c91262d
Daehan <dh16931@gmail.com>
2021-09-07 18:41:05 +0900

    feat: button disabled

--- components/common/Button.js
+++ components/common/Button.js
@@ -3,11 +3,19 @@ import { PrimaryBtn } from "../../styles/common-styles";
 
 const Btn = styled.button`
   ${PrimaryBtn}
+
+  :disabled {
+    cursor: not-allowed;
+    color: #ff7875;
+    text-decoration: line-through;
+    background: #fff;
+    border-color: #ff7875;
+  }
 `;
 
-const Button = ({ text, onClick = () => {}, name = "" }) => {
+const Button = ({ text, onClick = () => {}, name = "", disabled = false }) => {
   return (
-    <Btn name={name} onClick={() => onClick()}>
+    <Btn name={name} onClick={() => onClick()} disabled={disabled}>
       {text}
     </Btn>
   );
